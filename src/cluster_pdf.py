1540683b339eadf8c31101e7d0129b3b1c660634
mathu-kana <mathura.kana@hotmail.com>
2024-09-30 17:24:02 -0400

    Added more clustering algorithms on embeddings with class prediction stats scores
    BUG with get_features_from_loader: AttributeError: Can't pickle local object 'FeatureDataset.__init__.<locals>.<lambda>'

--- src/cluster_pdf.py
+++ src/cluster_pdf.py
@@ -1,26 +1,31 @@
 import os
 from PIL import Image
+import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 from matplotlib.backends.backend_pdf import PdfPages
 from pathlib import Path
 from sklearn.cluster import KMeans
 import pandas as pd
 from matplotlib.colors import ListedColormap
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import DBSCAN
 
-import loading_data as ld
 import utils
 import umap_features as ru
+import loading_data as ld
 
-def save_clusters_to_pdf(cluster_info, pdf_output_path):
-    """
-    Creates a pdf files displaying the image tiles that belong to each k-means cluster
-        cluster_info: pandas df with col 'ImagePath' and 'Cluster'
-
-    """
+def save_clusters_to_pdf(cluster_info, outfile):
+    '''
+    Creates a pdf files displaying the image tiles that belong to each cluster
+    Arguments
+    cluster_info: pandas DataFrame containing col 'ImagePath' and 'Cluster'
+    outfile: str of pdf file path to save
+    '''
     print("\nCreating PDF with image tiles ...")
     
     # Create a PdfPages object
-    with PdfPages(pdf_output_path) as pdf:
+    with PdfPages(outfile) as pdf:
         # Get unique clusters from 'Cluster' column in df
         unique_clusters = sorted(cluster_info['Cluster'].unique())
         
@@ -45,75 +50,140 @@ def save_clusters_to_pdf(cluster_info, pdf_output_path):
                 page_image_paths = image_paths[page * images_per_page:(page + 1) * images_per_page]
 
                 for i, (ax, image_path) in enumerate(zip(axes, page_image_paths)):
-                    # Adjust the file path to remove any redundant directory parts
-                    # image_path = image_path.replace(base_image_directory, "").lstrip("/")
-                    # full_image_path = os.path.join(base_image_directory, image_path)
-                    
                     # Load and display the image
-                    # img = Image.open(full_image_path)
                     img = Image.open(image_path)
                     ax.imshow(img)
                     ax.axis('off')  # Hide axes
                     
-                    # Get the base filename and remove the .jpg extension
-                    base_filename = os.path.splitext(os.path.basename(image_path))[0]
-                    
-                    # Display the filename without the extension in a smaller font
-                    ax.set_title(base_filename, fontsize=3)  # Smaller font size
+                    base_filename = os.path.splitext(os.path.basename(image_path))[0] # get base filename without extension
+                    ax.set_title(base_filename, fontsize=3) # display base filename
                 
                 # Hide any remaining empty subplots
                 for j in range(i + 1, len(axes)): # type: ignore
                     axes[j].axis('off')
 
-                # Save the current figure to the PDF with high DPI
-                pdf.savefig(fig, dpi=300)  # Increase DPI to 300 for better quality
+                pdf.savefig(fig, dpi=300) # save currrent figure to PDF, dpi quality
                 plt.close(fig)
 
-    print(f"Clusters saved to {pdf_output_path} as a PDF.")
+    print(f"Clusters saved to {outfile} as a PDF.")
     return
-"""KMEANS CLUSTERING ON UMAP PROJECTION"""
-def kmeans_clustering(umap_embedding, image_paths, annotations, seed, n_clusters,save_csv = False, cluster_csv_file_path = ''):
+
+"""CLUSTERING ON UMAP PROJECTION"""
+def kmeans_clustering(umap_embeddings, seed, n_clusters):
     kmeans = KMeans(n_clusters=n_clusters, random_state=seed)
-    clusters = kmeans.fit_predict(umap_embedding)
-    cluster_info = pd.DataFrame({'ImagePath': image_paths, 'Annotations': annotations,'Cluster': clusters})
-    if save_csv:
-        assert cluster_csv_file_path != ''
-        cluster_info.to_csv(cluster_csv_file_path, index=False)
-    return n_clusters, clusters, cluster_info
+    clusters = kmeans.fit_predict(umap_embeddings)
+    return clusters
 
-def plot_umap_for_kmeans(n_clusters, clusters, umap_embedding, umap_kmeans_output_path):
-    # Define a custom colormap with distinct colors for each cluster (max 20 colors, 20 clusters)
-    num_colors = min(n_clusters, 20)  # Ensure we don't exceed the number of available colors
-    colormap = plt.get_cmap('tab20', num_colors)
-    cluster_colors = colormap(range(num_colors))
+def agglomerative_clustering(umap_embeddings, n_clusters):
+    '''
+    Agglomerative clustering with provided n_clusters and Ward linkage
+    '''
+    agg_clustering = AgglomerativeClustering(n_clusters=n_clusters, linkage='ward')
+    clusters = agg_clustering.fit_predict(umap_embeddings)
+    return clusters
 
-    # Plot UMAP results
-    plt.figure(figsize=(10, 8))
-    plt.scatter(umap_embedding[:, 0], umap_embedding[:, 1], c=clusters, cmap=ListedColormap(cluster_colors), s=5)
+def agglomerative_clustering_no_n_clusters(umap_embeddings, distance_threshold=0):
+    '''
+    Agglomerative clustering without n_clusters, uses distance_threshold to form clusters
+    '''
+    agg_clustering = AgglomerativeClustering(n_clusters=None, distance_threshold=distance_threshold)
+    clusters = agg_clustering.fit_predict(umap_embeddings)
+    return clusters
+
+def dbscan_clustering(umap_embeddings, eps=0.5, min_samples=5):
+    dbscan = DBSCAN(eps=eps, min_samples=min_samples)
+    clusters = dbscan.fit_predict(umap_embeddings)
+    return clusters
+
+def get_clusters(cluster_type, embeddings, seed, n_clusters=10, eps=0.4, min_samples=500):
+    '''
+    Apply chosen clustering algorithm on embeddings
+    return
+    clusters: numpy.darray with cluster label associated with each embeddings (umap data point)
+    '''
+    print(f"Getting {cluster_type} clusters...")
+    if cluster_type == "k-means":
+        clusters = kmeans_clustering(embeddings, seed, n_clusters)
+    elif cluster_type == "agglomerative-clustering":
+        clusters = agglomerative_clustering_no_n_clusters(embeddings, distance_threshold=distance_threshold)
+        n_clusters = len(set(clusters)) # TODO verify is this is accurate
+    elif cluster_type == f"agglomerative-clustering-{str(n_clusters)}-clusters":
+        clusters = agglomerative_clustering(embeddings, n_clusters)
+    elif cluster_type == "dbscan":
+        clusters = dbscan_clustering(embeddings, eps=eps, min_samples=min_samples)
+        n_clusters = len(set(clusters)) # note: DBSCAN assigns -1 to noise points, this will still be considered as first cluster
+    else:
+        print(f"cluster_type: {cluster_type} is invalid. dbscan clustering is used with eps=0.4 and min_samples=500 (ddc-uc settings).")
+        clusters = dbscan_clustering(umap_embeddings, eps=eps, min_samples=min_samples) #ddc_uc settings
+
+    return clusters
+
+def plot_umap_for_kmeans(cluster_type, n_clusters, clusters, umap_embeddings, outfile):
+    # Colormap for clusters
+    if n_clusters >= 20:
+        colormap = plt.get_cmap('nipy_spectral', n_clusters) # 20+ clusters
+    elif n_clusters >= 10:
+      colormap = plt.get_cmap('tab20', n_clusters) # 10-19 clusters
+    else:
+        colormap = plt.get_cmap('tab10', n_clusters) # under 10 clusters
+
+    cluster_colors = colormap(np.linspace(0, 1, n_clusters)) # assign colors to clusters
+
+    # Plot parameters
+    plt.figure(figsize=(10, 10))
+    plt.scatter(umap_embeddings[:, 0], umap_embeddings[:, 1], c=clusters, cmap=ListedColormap(cluster_colors), s=0.4)
 
     # legend
-    handles = [plt.Line2D([0], [0], marker='o', color='w', markerfacecolor=cluster_colors[i], markersize=10, label=f'Cluster {i}') for i in range(n_clusters)]
-    plt.legend(handles=handles, title='Clusters')
+    num_cols_in_legend = 5
+    handles = [Line2D([0], [0], marker='o', color='w', markerfacecolor=cluster_colors[i], markersize=10, label=f'Cluster {i}') for i in range(n_clusters)]
+    plt.legend(handles=handles, title='Clusters', bbox_to_anchor=(0.5, -0.1), loc='upper center', ncol=num_cols_in_legend, fontsize='small')
 
-    plt.title(f'{tumor_type} UMAP Projection with k-means clustering on UMAP embeddings')
+    plt.title(f'{tumor_type} UMAP Projection with {cluster_type} clustering on UMAP embeddings')
     plt.xlabel('UMAP Dimension 1')
     plt.ylabel('UMAP Dimension 2')
-    plt.savefig(umap_kmeans_output_path)
-    plt.show()
+    plt.subplots_adjust(bottom=0.25)  # bottom margin
+    plt.savefig(outfile, bbox_inches='tight')
+    # plt.show()
     return
+
+def get_cases(image_paths):
+    #  case: everything before the last '_' in the base filename
+    return [os.path.basename(path).rsplit('_', 1)[0] for path in image_paths]
+
+"""OUTPUT CSV FILE"""
+def save_cluster_csv(image_paths, cases, annotations, clusters, save_csv = False, outfile = ''):
+    cluster_info = pd.DataFrame({'ImagePath': image_paths, 'Case': cases, 'Annotations': annotations,'Cluster': clusters})
+    if save_csv:
+        assert outfile != ''
+        cluster_info.to_csv(outfile, index=False)
+    return cluster_info
+
 if __name__ == "__main__":
+    # Parameters    
     tumor_type = "SCCOHT_1"
+    # tumor_type = "DDC_UC_1"
     run_id = f"{utils.get_time()[:10]}"
     seed = 99
     model_type = "ResNet"
     feature_directory = f"./features/{model_type}/{tumor_type}"
     size_of_feature_dataset = ld.get_size_of_dataset(directory=feature_directory, extension='jpg')
-    sample_size = 100
+    sample_size = size_of_feature_dataset # 100
     batch_size = 100
 
-    pdf_file = f"umap_{tumor_type}_{run_id}_{seed}_{sample_size}.pdf"
-    umap_kmeans_file = f"umap_{tumor_type}_{run_id}_{seed}_{sample_size}_kmeans.png"
-    csv_file = f"umap_{tumor_type}_{run_id}_{seed}_{sample_size}.csv"
+    """
+    n_clusters: determined based on umap
+    cluster_type: 
+        - k-means
+        - agglomerative-clustering
+        - agglomerative-clustering-{str(n_clusters)}-clusters
+        - dbscan
+    """
+    cluster_type = "dbscan"
+
+    # File paths
+    pdf_file = f"umap_{tumor_type}_{model_type}_{cluster_type}_{run_id}_{seed}_{sample_size}.pdf"
+    umap_kmeans_file = f"umap_{tumor_type}_{model_type}_{cluster_type}_{run_id}_{seed}_{sample_size}.png"
+    csv_file = f"umap_{tumor_type}_{model_type}_{cluster_type}_{run_id}_{seed}_{sample_size}.csv"
 
     results_directory = f"./results/clusters"
     Path(results_directory).mkdir(parents=True, exist_ok=True)
@@ -121,12 +191,32 @@ if __name__ == "__main__":
     umap_kmeans_output_path = os.path.join(results_directory, umap_kmeans_file)
     csv_output_path = os.path.join(results_directory, csv_file)
 
-    image_paths, feature_loader, classes = ru.get_features_from_disk(size_of_dataset=size_of_feature_dataset,model_type=model_type,tumor_type=tumor_type,seed=seed,sample_size=sample_size)
 
-    n_clusters = 3 if 'DDC_UC' in tumor_type else 2
+    # Clustering analysis
+    image_paths, feature_loader, classes = ru.get_features_from_disk(size_of_dataset=size_of_feature_dataset,model_type=model_type,tumor_type=tumor_type,sample_size=sample_size)
+    features_array, annotations = ru.get_features_from_loader(feature_loader,classes=classes)
+
     umap_embeddings = ru.generate_umap_annotation(feature_loader, seed, tumor_type,save_plot=False, tumor_classes=classes,normalizer=ru.feature_normalizer())
+    
+    # Clustering of UMAP embeddings
+    n_clusters = 10 # kmeans_clustering, agglometative clustering
+    distance_threshold=0 # agglomerative clustering without n_clusters
+    eps=0.4 # dbscan ddc-uc
+    min_samples=500 # dbscan ddc-uc
+    # eps=0.3 # dbscan sccoht
+    # min_samples=50 # dbscan sccoht
 
-    # UMAP dimension reduction with k-means clustering on umap_embeddings in legend
-    n_clusters, clusters, cluster_info = kmeans_clustering(umap_embeddings, image_paths,annotations, seed, n_clusters,save_csv= True, cluster_csv_file_path = csv_output_path)
-    plot_umap_for_kmeans(n_clusters, clusters, umap_embeddings, umap_kmeans_output_path)
-    save_clusters_to_pdf(cluster_info, pdf_output_path)
+    clusters = get_clusters(
+                            cluster_type=cluster_type,
+                            embeddings=umap_embeddings,
+                            seed=seed,
+                            n_clusters=n_clusters,
+                            eps=eps,
+                            min_samples=min_samples
+                            )
+    
+    
+    print("Plotting clustering umap...")
+    plot_umap_for_kmeans(cluster_type=cluster_type, n_clusters=n_clusters, clusters=clusters, umap_embeddings=umap_embeddings, outfile=umap_kmeans_output_path)
+
+    cluster_info = save_cluster_csv(image_paths, get_cases(image_paths), annotations, clusters, save_csv=True, outfile=csv_output_path)
--- src/stats_validation.py
+++ src/stats_validation.py
@@ -6,51 +6,57 @@ To get
     - f1-score 
 
 For testing purposes: reads & converts .csv file into pandas dataframe
-* Needs to take pandas df directly as an argument instead when integrated with 'resnet_umap.py'
-
 """
 
 import pandas as pd
 from sklearn.metrics import precision_score, accuracy_score, recall_score, f1_score
 
 
-# Create a dictionary mapping cluster # to a predicted class (normal/tumor or normal/undiff/well_diff)
 def create_cluster2pred_mapping(n_clusters, tumor_type):
+    '''
+    Create a dict mapping cluster label to a predicted class
+    '''
     cluster_to_pred = {}
     if "DDC_UC" in tumor_type:
-        print("Assign 'normal', 'undiff' or well_diff' to each cluster.")
+        print("\n\nAssign 'normal', 'undiff', 'well_diff', 'noise' or 'exclude' to each cluster.")
     else:
-        print("Assign 'normal' or 'tumor' to each cluster.")
+        print("\n\nAssign 'normal', 'tumor', 'noise' or 'exclude' to each cluster.")
 
-    for i in range(n_clusters):
+    for i in range(-1, n_clusters-1):
         while True: # will break if valid input is given, otherwise stays in loop to ask for input again
-            pred = input(f"Cluster {i} is: ").strip().lower() # gets which class each cluster corresponds to (normal/tumor (or undiff, well_diff))
+            pred = input(f"Cluster {i} is: ").strip().lower() # gets which class each cluster corresponds to (normal/tumor (or undiff, well_diff), noise, exclude)
             if "DDC_UC" in tumor_type:
-                if pred in ['normal', 'undiff', 'well_diff']: # checks if input is valid
+                if pred in ['normal', 'undiff', 'well_diff', 'noise', 'exclude']: # checks if input is valid
                     cluster_to_pred[i] = pred
                     break
                 else:
-                    print("Invalid input. Please enter 'normal', 'undiff' or 'well_diff'.")
+                    print("Invalid input. Please enter 'normal', 'undiff', 'well_diff', 'noise' or 'exclude'.")
             else:
-                if pred in ['normal', 'tumor']:
+                if pred in ['normal', 'tumor', 'noise', 'exclude']:
                     cluster_to_pred[i] = pred
                     break
                 else:
-                    print("Invalid input. Please enter 'normal' or 'tumor'.")
+                    print("Invalid input. Please enter 'normal', 'tumor', 'noise' or 'exclude'.")
     return cluster_to_pred
 
 
 def stats_validation(csv_file, n_clusters, tumor_type, output_file=None):
     df = pd.read_csv(csv_file) # temporary until testing is done and dataframe is passed as an argument
-    cluster_to_pred = create_cluster2pred_mapping(n_clusters, tumor_type)
+    cluster_to_pred = create_cluster2pred_mapping(n_clusters, tumor_type) # maps cluster label to predicted class
     print(f"\ncluster_pred map:{cluster_to_pred}")
 
     df['Predicted'] = df['Cluster'].map(cluster_to_pred) # added 'Predicted' col to df using the mapping from cluster to predicted class
+
+    # Remove rows where 'Predicted' is 'noise' or 'exclude'
+    df = df[~df['Predicted'].isin(['noise', 'exclude'])]
+
     if output_file:
         df.to_csv(output_file, index=False)
+        print(f"Predictions saved to output file {output_file}")
 
     # Calculate accuracy, precision, recall and f1 metrics
     accuracy = accuracy_score(df['Annotations'], df['Predicted'])
+
     # multi-class classification with Weighted-averaging: Useful when you have class imbalance and want to give more importance to the performance on larger classes
     if "DDC_UC" in tumor_type:
         precision = precision_score(df['Annotations'], df['Predicted'], average='weighted')
@@ -66,12 +72,17 @@ def stats_validation(csv_file, n_clusters, tumor_type, output_file=None):
     return accuracy, precision, recall, f1
 
 if __name__ == "__main__":
-
-    csv_file = "./results/umap_SCCOHT_1_2024-08-28_disk_k8_99_106039.csv"
-    n_clusters = 8
+    ''' SCCOHT '''
     tumor_type = "SCCOHT_1"
+    n_clusters = 13
+    csv_file = "./results/clusters/umap_SCCOHT_1_ResNet_dbscan_2024-09-21_99_good.csv"
+    output_file = f"./results/clusters/umap_SCCOHT_1_ResNet_dbscan_2024-09-21_99_good_prediction.csv"
 
-    output_file = f"./results/umap_SCCOHT_1_2024-08-28_disk_k8_99_106039_predictions.csv"
+    # ''' DDC-UC '''
+    # tumor_type = "DDC-UC_1"
+    # n_clusters = 7
+    # csv_file = "./results/clusters/umap_DDC_UC_1_ResNet_dbscan_2024-09-19_99_1_good.csv"
+    # output_file = f"./results/clusters/umap_DDC_UC_1_ResNet_dbscan_2024-09-19_99_1_good_prediction.csv"
 
     accuracy, precision, recall, f1 = stats_validation(csv_file, n_clusters, tumor_type, output_file)
     
@@ -79,9 +90,3 @@ if __name__ == "__main__":
     print(f"Accuracy: {accuracy:.4f}")
     print(f"Recall: {recall:.4f}")
     print(f"F1-Score: {f1:.4f}")
-
-
-
-
-
-
