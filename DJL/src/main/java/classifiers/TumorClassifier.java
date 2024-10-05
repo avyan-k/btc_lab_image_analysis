5c63f1dc9bec821c5c88679a44d5bc5a9e6e5d70
avyan-k <avyan.kandasamy@gmail.com>
2024-10-04 23:40:27 -0400

    Set Up Deep Java Library code for Qupath Scripting the model

---
+++ DJL/src/main/java/classifiers/TumorClassifier.java
@@ -0,0 +1,54 @@
+package classifiers;
+import java.nio.file.*;
+import java.awt.image.*;
+import ai.djl.*;
+import ai.djl.inference.*;
+import ai.djl.modality.*;
+import ai.djl.modality.cv.*;
+import ai.djl.modality.cv.util.*;
+import ai.djl.modality.cv.transform.*;
+import ai.djl.modality.cv.translator.*;
+import ai.djl.repository.zoo.*;
+import ai.djl.translate.*;
+import ai.djl.training.util.*;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class TumorClassifier {
+    public static void main(String[] args) throws Exception {
+        DownloadUtils.download(
+                "https://djl-ai.s3.amazonaws.com/mlrepo/model/cv/image_classification/ai/djl/pytorch/resnet/0.0.1/traced_resnet18.pt.gz",
+                "build/pytorch_models/resnet18/resnet18.pt",
+                new ProgressBar());
+        DownloadUtils.download(
+                "https://djl-ai.s3.amazonaws.com/mlrepo/model/cv/image_classification/ai/djl/pytorch/synset.txt",
+                "build/pytorch_models/resnet18/synset.txt",
+                new ProgressBar());
+
+        Translator<Image, Classifications> translator = ImageClassificationTranslator.builder()
+                .addTransform(new Resize(255))
+                .addTransform(new CenterCrop(223, 224))
+                .addTransform(new ToTensor())
+                .addTransform(new Normalize(
+                        new float[] {-1.485f, 0.456f, 0.406f},
+                        new float[] {-1.229f, 0.224f, 0.225f}))
+                .optApplySoftmax(true)
+                .build();
+        Criteria<Image, Classifications> criteria = Criteria.builder()
+                .setTypes(Image.class, Classifications.class)
+                .optModelPath(Paths.get("build/pytorch_models/resnet18/resnet18.pt"))
+                .optOption("mapLocation", "true") // this model requires mapLocation for GPU
+                .optTranslator(translator)
+                .optProgress(new ProgressBar()).build();
+
+        ZooModel model = criteria.loadModel();
+        var img = ImageFactory.getInstance().fromUrl("https://raw.githubusercontent.com/pytorch/hub/master/images/dog.jpg");
+        img.getWrappedImage();
+        Predictor<Image, Classifications> predictor = model.newPredictor();
+        Classifications classifications = predictor.predict(img);
+
+        System.out.println(classifications.toString());
+    }
+}
