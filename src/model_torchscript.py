2dd3be46708b742dacfe642d7443a73acd4a63df
avyan-k <avyan.kandasamy@gmail.com>
2024-10-07 14:10:16 -0400

    Removed DJL project, added torchscript saving

--- src/model_torchscript.py
+++ src/model_torchscript.py
@@ -1,4 +1,5 @@
 import torch
+from pathlib import Path
 
 import models as md
 import utils
@@ -10,7 +11,7 @@ if __name__ == "__main__":
     seed = 99
     DEVICE = utils.load_device(seed)
     torch.manual_seed(seed)
-
+    tumor_type = "DDC_UC_1"
 
     feature_classifier = md.Tumor_Classifier(layers=5, neurons_per_layer=64, dropout=0.0, input_neurons=1000, classes=3) 
     traced_feature_classifier = torch.jit.script(feature_classifier)
@@ -25,9 +26,8 @@ if __name__ == "__main__":
     print(traced_resnet_classifier.code)
     print(traced_resnet_classifier(x),resnet_classifier(x))
 
-    # accuracy = torchmetrics.Accuracy(task="multiclass", num_classes=3,average = 'weighted').to(DEVICE)
-    # for x,y_val in loader:
-    #     x = x.to(DEVICE)
-    #     y_val = y_val.to(DEVICE)
-    #     y_hat = resnet_classifier(x)
-    #     print(accuracy(y_hat,y_val))
+    traced_resnet_classifier.load_state_dict(torch.load('./results/training/models/ResNet_Tumor/DDC_UC_1/4-257-0.9653239250183105.pt', map_location = DEVICE,weights_only=True))
+
+    model_path = f"./results/training/torchscript_models/{str(type(resnet_classifier).__name__)}/{tumor_type}"
+    Path(model_path).mkdir(parents=True, exist_ok=True)
+    traced_resnet_classifier.save(model_path)
