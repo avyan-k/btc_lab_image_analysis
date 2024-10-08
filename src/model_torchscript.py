86c8c0f624a3cca5f42d63be4594a19a64f3dc92
avyan-k <avyan.kandasamy@gmail.com>
2024-10-08 19:36:12 -0400

    Fixed bug in torchscript saving

--- src/model_torchscript.py
+++ src/model_torchscript.py
@@ -1,5 +1,6 @@
 import torch
 from pathlib import Path
+import os
 
 import models as md
 import utils
@@ -26,8 +27,11 @@ if __name__ == "__main__":
     print(traced_resnet_classifier.code)
     print(traced_resnet_classifier(x),resnet_classifier(x))
 
-    traced_resnet_classifier.load_state_dict(torch.load('./results/training/models/ResNet_Tumor/DDC_UC_1/4-257-0.9653239250183105.pt', map_location = DEVICE,weights_only=True))
+    weight_path = './results/training/models/ResNet_Tumor/DDC_UC_1/1-771-0.9662657976150513.pt'
+    weight_name = os.path.basename(weight_path)
+    traced_resnet_classifier.load_state_dict(torch.load(weight_path, map_location = DEVICE,weights_only=True))
 
-    model_path = f"./results/training/torchscript_models/{str(type(resnet_classifier).__name__)}/{tumor_type}"
-    Path(model_path).mkdir(parents=True, exist_ok=True)
-    traced_resnet_classifier.save(model_path)
+    model_path = f"./results/training/torchscript_models/{str(type(resnet_classifier).__name__)}/{tumor_type}/{weight_name}"
+    print(os.path.dirname(model_path))
+    Path(os.path.dirname(model_path)).mkdir(parents=True, exist_ok=True)
+    torch.jit.save(traced_resnet_classifier,model_path)
