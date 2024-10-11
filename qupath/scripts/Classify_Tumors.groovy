2368fa035abda6e80533b8f44425fae947ff60ad
avyan-k <avyan.kandasamy@gmail.com>
2024-10-10 23:03:59 -0400

    Added WSInfer tiling TODO: Classify tiles based on confidence

--- qupath/scripts/Classify_Tumors.groovy
+++ qupath/scripts/Classify_Tumors.groovy
@@ -1,2 +1,3 @@
-String model = "local/DDC_UC_1-Unnormalized/torchscript_model.pt"
-selectAnnotations()
+String model = "local/DDC_UC_1-Unnormalized/torchscript_model.pt";
+createAnnotationsFromPixelClassifier("Full Tissue", 150000.0, 0.0, "SPLIT", "DELETE_EXISTING", "INCLUDE_IGNORED")
+selectAnnotations();
