40ad2f5f286a7f6126e920efc74c76e5f3618766
avyan-k <avyan.kandasamy@gmail.com>
2024-10-10 21:23:42 -0400

    Added Thresholder

---
+++ qupath/scripts/DJLLauchScript.bat
@@ -0,0 +1,3 @@
+set PATH=C:\Users\avyan\miniconda3\envs\torch-staintools-env;C:\Users\avyan\miniconda3\envs\torch-staintools-env\bin;C:\Users\avyan\miniconda3\envs\torch-staintools-env\lib;%PATH%
+
+"C:\Users\avyan\AppData\Local\QuPath-0.5.1\QuPath-0.5.1 (console).exe"
