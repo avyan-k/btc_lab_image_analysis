79fc0823fd6cac95eee783a6f901720e0dca660a
avyan-k <avyan.kandasamy@gmail.com>
2024-10-11 15:01:59 -0400

    Added normalized model annotations

--- qupath/scripts/Classify_Tumors.groovy
+++ qupath/scripts/Classify_Tumors.groovy
@@ -1,3 +1,27 @@
-String model = "local/DDC_UC_1-Unnormalized/torchscript_model.pt";
 createAnnotationsFromPixelClassifier("Full Tissue", 150000.0, 0.0, "SPLIT", "DELETE_EXISTING", "INCLUDE_IGNORED")
+
+baseThreshold = 0.7
+belowBaseThresholdClass = "Other"
+
 selectAnnotations();
+qupath.ext.wsinfer.WSInfer.runInference("DDC_UC_1-Unnormalized")
+def tiles = getTileObjects()
+tiles.each { t ->
+    def maximum = Collections.max(t.measurements.entrySet(), Map.Entry.comparingByValue())
+    if(maximum.getValue() < baseThreshold) {
+       t.classifications = [belowBaseThresholdClass]
+    }
+}
+runPlugin('qupath.lib.plugins.objects.TileClassificationsToAnnotationsPlugin', '{"pathClass":"All classes","deleteTiles":false,"clearAnnotations":false,"splitAnnotations":false}')
+def classes = tiles[0].measurements.keySet()
+for (tumorclass:classes) {
+    selectObjectsByClassification(tumorclass);
+    mergeSelectedAnnotations();
+    resetSelection();
+}
+selectObjectsByClassification("Other");
+mergeSelectedAnnotations()
+resetSelection()
+selectObjectsByClassification("Region*");
+mergeSelectedAnnotations()
+resetSelection()
