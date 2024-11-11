17ea5b91351b5171f40614707fee113b933a0dc0
avyan-k <avyan.kandasamy@gmail.com>
2024-11-11 14:42:16 -0500

    Added loading of UNI model

--- src/models.py
+++ src/models.py
@@ -1,9 +1,10 @@
 import torch
 import torch.nn as nn
 import timm
-from huggingface_hub import login
+from huggingface_hub import login, hf_hub_download
 import torch.nn.functional as F
 from uni import get_encoder
+import os
 
 import utils
 
@@ -54,19 +55,58 @@ class ResNet_Tumor(nn.Module):
         x = self.fc(x)
         return x
 
+class UNI_Tumor(nn.Module):
+    def __init__(self, classes=2, feature_classifier=None, UNI_pretrained = False):
+        super(UNI_Tumor, self).__init__()
+        if feature_classifier is None:
+            self.fc = Tumor_Classifier(
+                layers=5,
+                neurons_per_layer=64,
+                dropout=0,
+                input_neurons=1000,
+                classes=classes,
+            )
+        else:
+            self.fc = feature_classifier
+            self.uni = timm.create_model(
+                "vit_large_patch16_224", img_size=224, patch_size=16, init_values=1e-5, num_classes=0, dynamic_img_size=True, pretrained=False
+                )
+            if UNI_pretrained:
+                try:
+                    local_dir = "./assets/ckpts/vit_large_patch16_224.dinov2.uni_mass100k/"
+                    self.uni.load_state_dict(torch.load(os.path.join(local_dir, "pytorch_model.bin"), map_location="cpu"), strict=True)
+                except FileNotFoundError:
+                    download_UNI_model_weights()
+    def forward(self, x):
+        x = self.uni(x)
+        x = self.fc(x)
+        return x
+
+def download_UNI_model_weights():
+    try:
+        with open("hugging_face.txt") as f:
+            token = f.read()
+    except FileNotFoundError:
+        raise FileNotFoundError(
+            "Access token for hugging fase not found. Save as ./hugging_face.txt in root directory of project"
+        )
+    login(token)
+    model_dir = "./assets/ckpts/vit_large_patch16_224.dinov2.uni_mass100k/"
+    os.makedirs(model_dir, exist_ok=True)
+    hf_hub_download("MahmoodLab/UNI", filename="pytorch_model.bin", local_dir=model_dir, force_download=True)
 
 if __name__ == "__main__":
     model = ResNet_Tumor()
     utils.print_cuda_memory()
     seed = 99
     DEVICE = utils.load_device(seed)
-    try:
-        with open("hugging_face.txt") as f:
-            token = f.read()
-    except FileNotFoundError:
-        raise FileNotFoundError(
-            "Access token for hugging fase not found. Save as hugging_face.txt in root directory of project"
-        )
-    login(token)
     if str(DEVICE) != "cpu":
         model, transform = get_encoder(enc_name="uni", device=DEVICE)
+    classifier = Tumor_Classifier(
+                        layers=5,
+                        neurons_per_layer=64,
+                        dropout=0,
+                        input_neurons=1000,
+                        classes=2,
+                    )
+    classifier.load_state_dict(torch.load("x.pt",map_location='cpu',weights_only=True))
