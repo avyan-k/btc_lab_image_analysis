d565c9469d48b54bb5b1df6ceace870a47468cb5
avyan-k <avyan.kandasamy@gmail.com>
2025-04-12 15:01:31 -0400

    Ensure a GPU with enough free memory is available before running any CUDA code

--- src/utils.py
+++ src/utils.py
@@ -8,29 +8,44 @@ from tqdm import tqdm
 import re
 
 
-def load_device(seed: int = 99):
+def load_device(seed: int = 99, GPU_VRAM_request = 0.75):
     set_seed(seed)
     # if a macOs then use mps
     if torch.backends.mps.is_built():
         device = torch.device("mps")
+        print(f"Loading device as {device} with seed {seed}")
     # if a GPU is available, use it
     elif torch.cuda.is_available():
-        device = torch.device("cuda")
-
+        device,mem_ratio = get_free_device(GPU_VRAM_request)
+        print(f"Loading device as {device}, with {mem_ratio:.00%} free memory and seed {seed}")
     # revert to the default (CPU)
     else:
         device = torch.device("cpu")
-    print(f"Loading device as {device} with seed {seed}")
+        print(f"Loading device as {device} with seed {seed}")
     return device
 
+def get_free_device(mem_request):
+    device_count = torch.cuda.device_count()
+    free_cuda_memories = [0.0] * device_count
+    total_cuda_memories = [0.0] * device_count
+    cuda_memory_ratios = [0.0] * device_count
+    for dev_idx in range(device_count):
+        free_cuda_memories[dev_idx], total_cuda_memories[dev_idx] = torch.cuda.mem_get_info(torch.device(f"cuda:{dev_idx}"))
+        cuda_memory_ratios[dev_idx] = free_cuda_memories[dev_idx] / total_cuda_memories[dev_idx] if (total_cuda_memories[dev_idx]>0.0) else 0.0 
+    max_mem = max(range(device_count),key=lambda x:cuda_memory_ratios[x]) # argmax
+    if (cuda_memory_ratios[max_mem] > mem_request):
+        return torch.device(f"cuda:{max_mem}"),cuda_memory_ratios[max_mem]
+    else:
+        raise MemoryError(f"Unable to find GPU that satisfies memory request of {mem_request:.00%}. Device with max memory is {max_mem} with total memory {free_cuda_memories[max_mem]//1024**3} GBs and free memory {free_cuda_memories[max_mem]//1024**3} GBs")
 
-def set_seed(seed: int):
+
+def set_seed(seed: int,GPU_deterministic = False):
     torch.manual_seed(seed)
     np.random.seed(seed)
     random.seed(seed)
     # for cuda
     torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.deterministic = GPU_deterministic
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.enabled = False
 
@@ -43,7 +58,6 @@ def get_time():
 def rename_dir(path, src, dst):
     # convert to list so that we can change elements
     parts = list(path.parts)
-
     # replace part that matches src with dst
     if (
         type(src) is int
@@ -88,4 +102,5 @@ def remove_image_label_abbreviations():
 
 
 if __name__ == "__main__":
+    get_free_device(0.5)
     load_device(0)
